f81eaaf4285f87c8779eb58f4707b3f225cdc6e4
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-07 01:18:11 +0300

    Update ayar.php

--- ayar.php
+++ ayar.php
@@ -1,16 +1,16 @@
-<?php
-$Servername="localhost";
-$username="gamzesi1_1301";
-$sifre="******";
-$dbname="gamzesi1_1301";
-
-$baglan=mysqli_connect($Servername,$username,$sifre,$dbname);
-
-mysqli_set_charset($baglan,"UTF-8");
-mysqli_query($baglan,"SET NAMES UTF8");
-
-
-
-
-
-?>
+<?php
+$Servername="localhost";
+$username="gamzesi1_1301";
+$sifre="130199Ga.";
+$dbname="gamzesi1_1301";
+
+$baglan=mysqli_connect($Servername,$username,$sifre,$dbname);
+
+mysqli_set_charset($baglan,"UTF-8");
+mysqli_query($baglan,"SET NAMES UTF8");
+
+
+
+
+
+?>
