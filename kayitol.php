eb344ef48bad4ca4cc338abfb514eaaa64f93a1a
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-07 00:25:27 +0300

    Update kayitol.php

--- kayitol.php
+++ kayitol.php
@@ -2,6 +2,7 @@
 
 
 include ('ayar.php');
+require("class.phpmailer.php");
 
 $kullaniciad=$_GET["kadi"];
 $kullanicimail=$_GET["mailAdres"];
@@ -30,14 +31,31 @@ if($count>0){
     
     $adduser=mysqli_query($baglan,"INSERT INTO veteriner_kullanicilar(kadi,mailAdres,parola,dogrulamakodu,durum) VALUES ('$kullaniciad','$kullanicimail','$kullaniciparola','$dogrulamakodu','$durum')");
     
-    $git="http://www.gamzesirakaya.com/veterinerservis/aktifet.php?mailAdres=".$kullanicimail."&dogrulamakodu=".$dogrulamakodu."";
-    $to="gamze@gamzesirakaya.com";
-    $subject="Kullanici hesabi aktifleştirme"; 
-    $text="Merhaba Sayin".$kullaniciad."\n Sisteme giriş yapabilmeniz için onayınız gereklidir.<a href='".$git."'>Onayla</a>";
-    $from="From:info@kocveteriner.com ";
-   $from .="MIME-Version:1.0\r\n";
-    $from.="Content-Type:text/html; charset=UTF-8\r\n";
-    mail($to,$subject,$text,$from);
+    //$git="http://www.gamzesirakaya.com/veterinerservis/aktifet.php?mailAdres=".$kullanicimail."&dogrulamakodu=".$dogrulamakodu."";
+    $kullanicimail = new PHPMailer();
+$kullanicimail->IsSMTP();
+$kullanicimail->SMTPDebug = 1; // Hata ayıklama değişkeni: 1 = hata ve mesaj gösterir, 2 = sadece mesaj gösterir
+$kullanicimail->SMTPAuth = true; //SMTP doğrulama olmalı ve bu değer değişmemeli
+$kullanicimail->SMTPSecure = 'tls'; // Normal bağlantı için boş bırakın veya tls yazın, güvenli bağlantı kullanmak için ssl yazın
+$kullanicimail->Host = "furina.alastyr.com"; // Mail sunucusunun adresi (IP de olabilir)
+$kullanicimail->Port = 587; // Normal bağlantı için 587, güvenli bağlantı için 465 yazın
+$kullanicimail->IsHTML(true);
+//$kullanicimail->SetLanguage("tr", "phpmailer/language");
+$kullanicimail->CharSet  ="utf-8";
+    //$git="http://www.gamzesirakaya.com/veterinerservis/aktifet.php?mailAdres=".$kullanicimail."&dogrulamakodu=".$dogrulamakodu."";
+    $kullanicimail->Username = "gamze@gamzesirakaya.com"; // Gönderici adresiniz (e-posta adresiniz)
+$kullanicimail->Password = "Gamze1234."; // Mail adresimizin sifresi
+$kullanicimail->SetFrom("gamze@gamzesirakaya.com", "Gamze sırakaya"); // Mail atıldığında gorulecek isim ve email
+$kullanicimail->AddAddress("sirakayagamze3@gmail.com"); // Mailin gönderileceği alıcı adres
+$kullanicimail->Subject = "merhaba"; // Email konu başlığı
+$kullanicimail->Body = "Merhaba Sayin".$kullaniciad."\n Sisteme giriş yapabilmeniz için onayınız gereklidir"; // Mailin içeriği
+if(!$kullanicimail->Send()){
+  echo "Email Gönderim Hatasi: ".$kullanicimail->ErrorInfo;
+} else {
+  echo "Email Gonderildi";
+}
+   
+   
 
 }
 
