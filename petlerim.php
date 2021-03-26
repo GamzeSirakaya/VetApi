4706e06759d2085a2d2c9ce87dd4156c9b028a00
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-26 21:10:53 +0300

    Update petlerim.php

--- petlerim.php
+++ petlerim.php
@@ -1,6 +1,6 @@
 <?php
 include('ayar.php');
-$mus_id=30;
+$mus_id=$_POST["mus_id"];
 $sorgu=mysqli_query($baglan,"select * from veteriner_pet_listesi where mus_id=$mus_id");
 $count=mysqli_num_rows($sorgu);
 class petClass{
@@ -16,8 +16,35 @@ $sayac = 0;
 
 if($count>0){
     echo("[");
-   // while($bilgi=mysqli_fetch_assoc($sorgu))
+    while($bilgi=mysqli_fetch_assoc($sorgu)){
+        $sayac=$sayac+1;
+       $pet->petid=$bilgi["id"];
+        $pet->petisim=$bilgi["pet_isim"];
+        $pet->petresim=$bilgi["pet_resim"];
+        $pet->pettur=$bilgi["pet_tur"];
+        $pet-> petcins=$bilgi["pet_cins"];
+        $tf=true;
+            echo(json_encode($pet));
+            if($count>$sayac){
+                echo(",");
+            }
+    }
+    echo("]");
 }else{
+     echo("[");
+   
+      
+       $pet->petid=null;
+        $pet->petisim=null;
+        $pet->petresim=null;
+        $pet->pettur=null;
+        $pet-> petcins=null;
+        $tf=false;
+        echo(json_encode($pet));
+         
+            
+    
+    echo("]");
     
 }
 
