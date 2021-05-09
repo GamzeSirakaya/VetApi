b88d70ce8603c7d3f07cb3a81f19475ab58ac373
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-05-09 22:03:17 +0300

    Update sorusor.php

--- sorusor.php
+++ sorusor.php
@@ -1,7 +1,7 @@
 <?php
 include("ayar.php");
-$mus_id="30";
-$soru = "asi fiyati öğrenebilirmiyim ?";
+$mus_id=$_POST["mus_id"];
+$soru = $_POST["soru"];
 $ekle =mysqli_query($baglan,"insert into veteriner_sorular(mus_id,soru,durum) values ('$mus_id','$soru','0')");
 
 class ekleme {
@@ -15,12 +15,12 @@ if($ekle){
     
     $ekle->text="Sorunuz ilgili veterinere ulaşmıştır . Cevabını bir zaman sonra cevaplar butonundan görebilirsiniz.";
     $ekle-> tf=true;
-    echo(json-encode($ekle));
+    echo(json_encode($ekle));
     
 }else{
     $ekle->text="Sorunuz gönderilirken bir hata meydana geld,i. Lütfen daha sonra tekrar deneyin";
     $ekle-> tf=false;
-   echo(json-encode($ekle));
+   echo(json_encode($ekle));
 }
 
 ?>
